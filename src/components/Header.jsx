08dc62ac3b20f434a1ad1c120f111f9c43084149
Binadi Laknara <binadi.20240153@iit.ac.lk>
2025-12-31 02:40:10 +0530

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import propertiesData from "../data/properties.json";
 import "./Header.css";
 
 function Header() {
-  const [firstPropertyId, setFirstPropertyId] = useState("");
-
-  useEffect(() => {
-    if (propertiesData.properties.length > 0) {
-      setFirstPropertyId(propertiesData.properties[0].id);
-    }
-  }, []);
+  const [firstPropertyId] = useState(
+    propertiesData.properties.length > 0 ? propertiesData.properties[0].id : ""
+  );
 
   return (
     <AppBar position="static">
